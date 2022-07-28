264a3956a824844daf04bfa861ddd129074f119b
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-28 16:44:48 -0700

    Make changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
   <div className="nav-wrapper">
-    <NavLink className="nav-title" to="/">
-      BookStore CMS
-    </NavLink>
-    <ul className="nav-links">
-      <li className="nav-link">
-        <NavLink className="nav-item" activeStyle={{ color: 'blue' }} to="/">
-          BOOKS
-        </NavLink>
-      </li>
-      <li className="nav-link">
-        <NavLink className="nav-item" activeStyle={{ color: 'blue' }} to="/category">
-          CATEGORY
-        </NavLink>
-      </li>
-    </ul>
+    <div>
+      <NavLink className="nav-title" to="/">
+        BookStore CMS
+      </NavLink>
+    </div>
+
+    <div>
+      <ul className="nav-links">
+        <li className="nav-link">
+          <NavLink className="nav-item" activeStyle={{ color: 'blue' }} to="/">
+            BOOKS
+          </NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink className="nav-item" activeStyle={{ color: 'blue' }} to="/category">
+            CATEGORY
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div className="icon">
+      <FontAwesomeIcon icon={faUser} />
+    </div>
   </div>
 );
 export default Navbar;
