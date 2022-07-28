6d32e771f878d12f92981b01acb573ee8aca903a
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-28 11:19:10 -0700

    Set up url for API

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -1,51 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
-
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
-
-export const AddBook = (book) => ({
-  type: ADD_BOOK,
-  book,
-});
-
-export const RemoveBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
-});
-
-const bookList = [
-  {
-    title: 'Harry Potter',
-    author: 'J.K Rowling',
-    id: uuidv4(),
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer',
-    id: uuidv4(),
-  },
-  {
-    title: 'Thor',
-    author: 'Stan Lee',
-    id: uuidv4(),
-  },
-];
+const bookList = [];
 
 const BooksReducer = (state = bookList, action) => {
   switch (action.type) {
-    case ADD_BOOK: {
-      const addedBook = {
-        id: uuidv4(),
-        ...action.book,
-      };
-      return [
-        ...state, addedBook,
-      ];
+    case 'bookstore/ADD_ASYNC_BOOK/fulfilled': {
+      return [...state, action.payload.book];
     }
-
-    case REMOVE_BOOK:
-      return state.filter((books) => books.id !== action.id);
-
+    case 'bookstore/DELETE_ASYNC_BOOK/fulfilled': {
+      return state.filter((book) => book.item_id !== action.payload.id);
+    }
+    case 'bookstore/GET_ASYNC_BOOK/fulfilled': {
+      return action.payload;
+    }
     default:
       return state;
   }
